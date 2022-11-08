93c0fd44a6f9d9176395c2753480e86308c42c79
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-08 10:20:30 -0500

    haciendo estáticos los valores de constante

--- recipe app/recipe app/Resources/Constants.swift
+++ recipe app/recipe app/Resources/Constants.swift
@@ -9,11 +9,11 @@ import Foundation
 
 struct Constants {
     struct URL {
-        let mainUrl: String = "http://demo4093731.mockable.io/"
+        static let mainUrl: String = "http://demo4093731.mockable.io/"
     }
     
     struct Endpoints {
-        let recipes: String = "recipe"
-        let recipeDetail: String = ""
+        static let recipes: String = "recipe"
+        static let recipeDetail: String = ""
     }
 }
