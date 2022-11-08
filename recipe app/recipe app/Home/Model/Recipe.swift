2b101341c5300b3370349f80f3a26958ea429a62
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-08 09:30:11 -0500

    Agregando recipe model

---
+++ recipe app/recipe app/Home/Model/Recipe.swift
@@ -0,0 +1,32 @@
+//
+//  Recipe.swift
+//  recipe app
+//
+//  Created by FABRIZIO on 8/11/22.
+//
+
+import Foundation
+
+struct Recipes: Codable {
+    let recipeList: [Recipe]
+    
+    enum CodingKeys: String, CodingKey {
+        case recipeList = "results"
+    }
+}
+
+struct Recipe: Codable {
+    let name: String
+    let dificulty: String
+    let duration: Double
+    let originContry: String
+    let image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "recipe_name"
+        case dificulty = "recipe_dificulty"
+        case duration = "recipe_duration"
+        case originContry = "recipe_country"
+        case image = "recipe_image"
+    }
+}
