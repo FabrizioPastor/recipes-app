3d48f4d58f591e6d1b31f565e62ddbb4a3ac1ca2
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-08 09:37:34 -0500

    Agregando Constants y ApiManager

---
+++ recipe app/recipe app/Resources/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  recipe app
+//
+//  Created by FABRIZIO on 8/11/22.
+//
+
+import Foundation
+
+struct Constants {
+    struct URL {
+        let mainUrl: String = "http://demo4093731.mockable.io/"
+    }
+    
+    struct Endpoints {
+        let recipes: String = "recipe"
+        let recipeDetail: String = ""
+    }
+}
---
+++ recipe app/recipe app/Resources/Networking/ApiManager.swift
@@ -0,0 +1,14 @@
+//
+//  ApiManager.swift
+//  recipe app
+//
+//  Created by FABRIZIO on 8/11/22.
+//
+
+import Foundation
+
+struct ApiManager {
+    func getRecipes() {
+        
+    }
+}
