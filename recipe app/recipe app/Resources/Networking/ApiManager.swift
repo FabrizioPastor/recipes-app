df39ecb4d083ac1adcc7e97a9eb46d53c46d6d68
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-08 10:20:43 -0500

    Implementando getRecipes

--- recipe app/recipe app/Resources/Networking/ApiManager.swift
+++ recipe app/recipe app/Resources/Networking/ApiManager.swift
@@ -6,9 +6,29 @@
 //
 
 import Foundation
+import RxSwift
 
 struct ApiManager {
-    func getRecipes() {
-        
+    func getRecipes() -> Observable<[Recipe]> {
+        return Observable.create { observer in
+            guard let url = URL(string: Constants.URL.mainUrl + Constants.Endpoints.recipes) else { fatalError("No se pudo crear al URL") }
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { data, response, error in
+                guard let data = data else { return }
+                do {
+                    let recipes = try JSONDecoder().decode(Recipes.self, from: data)
+                    observer.onNext(recipes.recipeList)
+                } catch {
+                    observer.onError(error)
+                    print("getRecipes(): \(error.localizedDescription)")
+                }
+            }
+            
+            task.resume()
+            
+            return Disposables.create {
+                session.finishTasksAndInvalidate()
+            }
+        }
     }
 }
