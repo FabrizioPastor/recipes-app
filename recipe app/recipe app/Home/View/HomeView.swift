3e48e3e4f62ec2d4dc2d5391ddcbd10162e159c1
FabrizioPastor <fabriziopastors@outlook.com>
2022-11-07 23:11:44 -0500

    Prueba levantar HomeView

--- recipe app/recipe app/Home/View/HomeView.swift
+++ recipe app/recipe app/Home/View/HomeView.swift
@@ -11,8 +11,7 @@ class HomeView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor =  .red
     }
 
 
